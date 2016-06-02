2ab292efc252044ce41fcef4ef1687589ad96f1a
Jacob Jewell <jacobjewell@eflexsystems.com>
2016-06-01 20:00:56 -0400

    experiment

--- addon/components/x-toggle/component.js
+++ addon/components/x-toggle/component.js
@@ -10,22 +10,7 @@ export default Ember.Component.extend({
   classNames: ['x-toggle-container'],
   disabled: false,
   value: false,
-  toggled: false,
   name: 'default',
-
-  onLabelValue: computed('onLabel', function () {
-    var on = this.get('onLabel');
-    var index = on.indexOf(':');
-
-    return index > -1 ? on.substr(0, index) : on;
-  }),
-
-  offLabelValue: computed('offLabel', function () {
-    var off = this.get('offLabel');
-    var index = off.indexOf(':');
-
-    return index > -1 ? off.substr(0, index) : off;
-  }),
 
   themeClass: computed('theme', function () {
     var theme = this.get('theme') || 'default';
@@ -37,41 +22,15 @@ export default Ember.Component.extend({
     return this.get('elementId') + '-x-toggle';
   }),
 
-  didUpdateAttrs: function({newAttrs, oldAttrs}) {
-    if (!oldAttrs) {
-      oldAttrs = {};
-    }
-
-    var offIndex = this.get('offLabel').indexOf(':');
-    var onIndex = this.get('onLabel').indexOf(':');
-    var offState = offIndex > -1 ? this.get('offLabel').substr(offIndex + 1) : false;
-    var onState = onIndex > -1 ? this.get('onLabel').substr(onIndex + 1) : true;
-
-    if (newAttrs.toggled !== oldAttrs.toggled) {
-      if (newAttrs.toggled === false) {
-        newAttrs.value = offState;
-      } else {
-        newAttrs.value = onState;
-      }
-    } else if (newAttrs.value !== oldAttrs.value) {
-      if (newAttrs.value === onState) {
-        newAttrs.toggled = true;
-      } else {
-        newAttrs.toggled = false;
-        newAttrs.value = offState;
-      }
-    }
-
-    this._super(...arguments);
-  },
-
   click(event) {
     event.stopPropagation();
   },
 
   actions: {
-    checkedChanged: function(e) {
-      this.sendAction('toggle', e);
+    checkedChanged: function(value) {
+      if (this.attrs.onToggle) {
+        this.attrs.onToggle(value)
+      }
     }
   }
 });
